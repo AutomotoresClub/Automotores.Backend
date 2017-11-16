2162668be4274d408df1ea841456cfc2caadbeb4
Alejandro Garay Cuellar <alejandro.garay9454@hotmail.com>
2017-11-16 12:04:51 -0500

    Se genera la migracion para llenar la tabla de dias de la semana

---
+++ Migrations/20171116170128_PopulateDiasSemana.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Automotores.Backend.Migrations
+{
+    public partial class PopulateDiasSemana : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO DiasSemana (Nombre) Values ('Lunes')");
+            migrationBuilder.Sql("INSERT INTO DiasSemana (Nombre) Values ('Martes')");
+            migrationBuilder.Sql("INSERT INTO DiasSemana (Nombre) Values ('Miercoles')");
+            migrationBuilder.Sql("INSERT INTO DiasSemana (Nombre) Values ('Jueves')");
+            migrationBuilder.Sql("INSERT INTO DiasSemana (Nombre) Values ('Viernes')");
+            migrationBuilder.Sql("INSERT INTO DiasSemana (Nombre) Values ('Sabado')");
+            migrationBuilder.Sql("INSERT INTO DiasSemana (Nombre) Values ('Domingo')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM DiasSemana");
+        }
+    }
+}
